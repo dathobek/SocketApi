a709c24f9f3b557f30e145a377bf963c6062fa96
David Athobe <dathobek@gmail.com>
2018-07-25 09:21:11 +0100

    express-initiated

---
+++ socketApi/api.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+})
+
+io.on('connection',function(socket){
+    console.log('user connected');
+
+    //disconnect user
+    socket.io('disconnect',function(){
+        console.log('hey,You are disconnected');
+    })
+})
+
+http.listen(3500,function(){
+    console.log('hey am listening!')
+})
